25f5b876b25f63f926d2ad1ff948ff2036e4672a
Abdi Hersi <abdi_shakur23@hotmail.com>
2023-05-04 11:54:56 -0600

    created search bar component

---
+++ src/components/api.js
@@ -0,0 +1,8 @@
+export const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5d6b4c7b2emsh1304bcadf5a1e8bp1fecd2jsn75d78d812fb8",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
